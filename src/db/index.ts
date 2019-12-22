c75ea60cc78ef19664931be4586b3f0196945074
Bohdan Onatsky <15bogdan17@gmail.com>
2019-12-22 17:49:01 +0200

    Add clusters to nodejs (#4)

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,22 @@
 import { createConnection } from 'typeorm';
+import {
+  database,
+  host,
+  password,
+  port,
+  type,
+  username
+} from '../config/database';
+import UserT from './entity/user';
 
 export default (async function instance() {
-  await createConnection();
+  await createConnection({
+    type,
+    host,
+    port,
+    username,
+    password,
+    database,
+    entities: [UserT]
+  });
 })();
