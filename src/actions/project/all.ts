f27b10ed4b17bc952e477287f20d6b2bcae6e5f0
rammfall <15bogdan17@gmail.com>
2020-03-14 19:33:03 +0200

    Update dependencies Add helpers FIx db migrations

--- src/actions/project/all.ts
+++ src/actions/project/all.ts
@@ -4,9 +4,10 @@ import all from './methods/all';
 
 export default async (req: Request, res: Response) => {
   const { id } = req.body;
+  const { take, skip } = req.query;
 
   try {
-    const projects = await all(id);
+    const projects = await all(id, take, skip);
 
     res.json({ projects });
   } catch (e) {
--- src/actions/project/methods/all.ts
+++ src/actions/project/methods/all.ts
@@ -1,11 +1,20 @@
 import User from '../../../db/entity/user';
 import Project from '../../../db/entity/project';
 
-export default async (id: number) => {
+export default async (id: number, take: number = 20, skip: number = 0) => {
   const user = await User.findOne({ id });
 
   if (user) {
-    const projects: Project[] = await Project.find({ user });
+    const projects: Project[] = await Project.find({
+      where: {
+        user
+      },
+      take,
+      skip,
+      order: {
+        id: 'DESC'
+      }
+    });
 
     return projects;
   }
